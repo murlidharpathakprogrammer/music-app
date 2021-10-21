a72dbcab03d5a89b2a82b7a0d12a3b85e269d897
murlidharpathakprogrammer <murlidharpathakprogrammer@gmail.com>
2021-10-21 21:27:32 +0530

    COMMITTING

---
+++ src/app/my-music-list/my-music-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-music-list',
+  templateUrl: './my-music-list.component.html',
+  styleUrls: ['./my-music-list.component.css']
+})
+export class MyMusicListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/recent-plays/recent-plays.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-plays',
+  templateUrl: './recent-plays.component.html',
+  styleUrls: ['./recent-plays.component.css']
+})
+export class RecentPlaysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
